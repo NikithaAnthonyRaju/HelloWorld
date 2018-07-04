2ff9d5f61174f2b6fa06581320edf3ea55cdd895
Nikitha <nikitha.a@dxc.com>
2018-07-04 12:52:32 +0530

    Latest Codes

---
+++ src/reverseSentance.java
@@ -0,0 +1,24 @@
+
+public class reverseSentance {
+
+	public static void main(String[] args) {
+		
+		reverseString("My name is Nikitha");
+
+	}
+	
+	public static String reverseString(String line) { 
+		if (line.trim().isEmpty()) { 
+			return line; 
+			} 
+		StringBuilder reverse = new StringBuilder(); 
+		String[] sa = line.trim().split("\\s"); 
+		for (int i = sa.length - 1; i >= 0; i--) { 
+			reverse.append(sa[i]); 
+			reverse.append(' '); 
+			} 
+		return reverse.toString().trim(); 
+	}
+
+
+}
---
+++ src/squareRoot.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class squareRoot {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number to find square root in Java : ");
+	      
+        //getting input number from user to calculate square root
+        double square = scanner.nextDouble();
+        
+        //getting square root of a number in Java
+        double squareRoot = Math.sqrt(square);
+        
+        System.out.printf("Square root of number: %f is : %f %n" , square, squareRoot);
+
+        
+	}
+
+}
