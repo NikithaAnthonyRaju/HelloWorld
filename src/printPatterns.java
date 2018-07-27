3319214bddd2ef44c917f07b4018636cd3145621
Nikitha <nikitha.a@dxc.com>
2018-07-27 20:47:50 +0530

    Commit

--- src/printPatterns.java
+++ src/printPatterns.java
@@ -22,14 +22,13 @@ public class printPatterns {
 	}
 	
 	public static void patternNum(int n){
+		int num =1;
 		
-		
-		for(int i=0; i<n; i++){
-			int num =1;
+		for(int i=0; i<n; i++){			
 						
 			for(int j=0; j<=i; j++){
 				System.out.print(num + " ");
-				num++;
+//				num++;
 			}
 			System.out.println();
 		}		
